8f8d9a1dbc931e5251d6727afc23df14b1430b1f
ujjwal <ujjwal01kumar@gmail.com>
2016-02-11 18:38:43 -0500

    switch case

--- ex1/ex12.c
+++ ex1/ex12.c
@@ -8,7 +8,7 @@ int main(int argc,char *argv[]){
 	printf("You have only one argument.You suck\n");
 	}else if(argc>1 && argc<4){
 		printf("Your arguments are:\n");
-		for(int i=0;i<3;i++){
+		for(int i=1;i<3;i++){
 			printf("argument %d: %s\n",i,argv[i]);
 		}
 		printf("\n");
