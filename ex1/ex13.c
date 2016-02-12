3abe4c7b81f5d8efae7a10c5f49ba5310c92f52c
ujjwal <ujjwal01kumar@gmail.com>
2016-02-12 01:12:17 -0500

    Functions

--- ex1/ex13.c
+++ ex1/ex13.c
@@ -2,47 +2,47 @@
 
 int main(int argc,char *argv[]){
 
-	if(argc !=2){
-		printf("You need only one argument\n");
-		//This is how you abort a program
-		return 0;
-	}
-	printf("%c\n",argv[0][0] );
+    if(argc !=2){
+        printf("You need only one argument\n");
+        //This is how you abort a program
+        return 0;
+    }
+    printf("%c\n",argv[0][4] );
     int i=0;
 
     for(i=0;argv[1][i];i++){
 
-    	char letter=argv[1][i];
+        char letter=argv[1][i];
 
-    	switch(letter){
-    		case 'a':
-    		case 'A':
-    		printf("%d 'A'\n",i );
-    		break;
+        switch(letter){
+            case 'a':
+            case 'A':
+            printf("%d 'A'\n",i );
+            break;
 
-    		case 'e':
-    		case 'E':
-    		printf("%d 'E'\n",i );
-    		break;
+            case 'e':
+            case 'E':
+            printf("%d 'E'\n",i );
+            break;
 
-    		case 'i':
-    		case 'I':
-    		printf("%d 'I'\n",i );
-    		break;
+            case 'i':
+            case 'I':
+            printf("%d 'I'\n",i );
+            break;
 
-    		case 'o':
-    		case 'O':
-    		printf("%d 'O'\n",i );
-    		break;
+            case 'o':
+            case 'O':
+            printf("%d 'O'\n",i );
+            break;
 
-    		case 'u':
-    		case 'U':
-    		printf("%d 'U'\n",i);
-    		break;
+            case 'u':
+            case 'U':
+            printf("%d 'U'\n",i);
+            break;
 
-    		default:
-    		printf("%d %c is not a vowel.\n",i,letter );
-    	}
+            default:
+            printf("%d %c is not a vowel.\n",i,letter );
+        }
     }
     return 0;
 }
